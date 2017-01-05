c6b5e9447c8b4941cc1c790ceaab682c3da8f5ba
Joel Junior <junior.tads@gmail.com>
2017-01-05 02:28:10 +0000

    Example connect ES on AWS

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.info("Hello word");
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'https://search-workshop-gft-i63jgkynlr5auoufpgsc5u66ja.us-west-2.es.amazonaws.com',
+  //log: 'trace'
+  
+});
+
+client.ping({
+  // ping usually has a 3000ms timeout
+  requestTimeout: Infinity
+}, function (error) {
+  if (error) {
+    console.trace('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
+client.search({
+  index: 'bank',
+  q: 'firstname:Mcgee'
+}, function (error, response) {
+   console.info(response.hits.hits[0]._source)
+});
